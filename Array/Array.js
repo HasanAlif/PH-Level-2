750dee6bcafb962268f82cfe56b5e961e146a917
HasanAlif <alifalmehedihasan@gmail.com>
2025-11-02 21:34:05 +0600

    Practice Day-1

---
+++ Array/Array.js
@@ -0,0 +1,16 @@
+const numbers = [40, 100, 1, 5, 25, 10];
+const fruits = ["Banana", "apple", "Cherry", "date"];
+
+const sortedNumber = numbers.sort((a, b) => a - b);
+
+console.log(numbers);
+console.log(sortedNumber);
+
+const tagsFromPosts = [
+    ["javascript", "react", "css"],
+    ["node", "express"],
+    ["css", "html", "react"],
+];
+
+const currentUserRoles = ["user", "editor"];
+const featureAccessRoles = ["admin", "manager"];
