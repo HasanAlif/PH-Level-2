8cec392f622c9c473a001b5597aeebf8dcf1143f
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2025-11-19 11:29:11 +0600

    nonPrimitive data types

---
+++ TypeScript/src/nonPrimitive.ts
@@ -0,0 +1,16 @@
+// Array, Object
+
+let bazarList: string[] = ["eggs", "milk", "butter"];
+
+let mixedArray: (string | number)[] = ["apple", 10, "banana", 20];
+
+let coordinates: [number, number] = [10, 20];
+
+let couple: [string, string] = ["John", "Jane"];
+
+let nameRoll: [string, number][] = [
+  ["Alice", 1],
+  ["Bob", 2],
+];
+
+let destination: [string, string, number] = ["Paris", "France", 5];
