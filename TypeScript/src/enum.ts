0014203b4bb27d7eda41145c75ed01bb3dd436bc
HasanAlif <alifalmehedihasan@gmail.com>
2025-11-25 10:20:15 +0600

    Enum in TypeScript

---
+++ TypeScript/src/enum.ts
@@ -0,0 +1,36 @@
+// enum
+
+// set of fixed string values
+
+// type UserRoles = "Admin" | "User" | "Guest";
+
+// const canEdit = (role: UserRoles) => {
+//   if (role === "Admin" || role === "User") {
+//     return true;
+//   } else return;
+// };
+
+// const isEditAllowed = canEdit("Admin");
+
+// console.log(isEditAllowed); // true
+
+// enum UserRoles {
+//   Admin = "Admin",
+//   User = "User",
+//   Guest = "Guest",
+// }
+
+const UserRoles = {
+  Admin: "Admin",
+  User: "User",
+  Guest: "Guest",
+} as const;
+
+const canEdit = (role: UserRoles) => {
+  if (role === UserRoles.Admin || role === UserRoles.User) {
+    return true;
+  } else return;
+};
+
+const isEditAllowed = canEdit(UserRoles.Admin);
+console.log(isEditAllowed); // true
