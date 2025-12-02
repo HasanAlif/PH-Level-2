6a95a198cca8d2e65789fed52d710b42c5d9a940
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2025-12-02 19:44:24 +0600

    CRUD Operation in Todos

--- Server_With_Express/src/server.ts
+++ Server_With_Express/src/server.ts
@@ -198,6 +198,130 @@ app.delete("/users/:id", async (req: Request, res: Response) => {
   }
 });
 
+// Part of Todos
+
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title, description, is_completed, due_date } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id, title, description, is_completed, due_date) VALUES($1, $2, $3, $4, $5) RETURNING *`,
+      [user_id, title, description, is_completed, due_date]
+    );
+    console.log(result.rows[0]);
+    res.status(201).json({
+      success: true,
+      message: `Todo "${title}" created for user ID ${user_id}.`,
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+    res.status(200).json({
+      success: true,
+      message: "Todos retrieved successfully.",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.get("/todos/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(`SELECT * FROM todos WHERE id = $1`, [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: `Todo with id ${id} not found.`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: `Todo with id ${id} retrieved successfully.`,
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.put("/todos/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { title, description, is_completed, due_date } = req.body;
+  try {
+    const existingTodo = await pool.query(`SELECT * FROM todos WHERE id = $1`, [
+      id,
+    ]);
+    if (existingTodo.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: `Todo with id ${id} not found.`,
+      });
+    }
+
+    const result = await pool.query(
+      `UPDATE todos SET title=$1, description=$2, is_completed=$3, due_date=$4, updated_at=Now() WHERE id=$5 RETURNING *`,
+      [title, description, is_completed, due_date, id]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: `Todo with id ${id} updated successfully.`,
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+app.delete("/todos/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const existingTodo = await pool.query(`SELECT * FROM todos WHERE id = $1`, [
+      id,
+    ]);
+    if (existingTodo.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: `Todo with id ${id} not found.`,
+      });
+    }
+
+    await pool.query(`DELETE FROM todos WHERE id = $1`, [id]);
+
+    res.status(200).json({
+      success: true,
+      message: `Todo with id ${id} deleted successfully.`,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
